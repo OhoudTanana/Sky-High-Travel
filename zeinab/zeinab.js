304667ce6fef3eddcab4884b311367e2b6b78e51
Ohoud Tanana <ohoud-tanana@live.com>
2023-10-17 20:02:37 +0300

    adding files to the repository

---
+++ zeinab/zeinab.js
@@ -0,0 +1,213 @@
+
+
+function validateform(){
+var name1;
+name1=document.getElementById("myname");
+if (name1.value.trim()=="")
+{
+	alert("Name field cannot be empty! ");
+return false;
+}
+
+var email1;
+email1=document.getElementById("myemail");
+if(email1.value.trim()==""){
+	alert("Email field cannot be empty!");
+	return false;
+}
+
+	
+}
+
+function check(){
+	alert("Thank You For your Submittion!Your review has been successfully submitted.");
+}
+
+function hoverout(img){
+img.src="pizza2.jpg"
+}
+function hover(img){
+img.src="pizza1.jpg"
+}
+
+function imageout2(img){
+	img.src="harees1.jpg"
+}
+function image2(img){
+	img.src="harees2.jpg"
+}
+
+function imageout3(img){
+	img.src="machbous1.jpg"
+}
+function image3(img){
+	img.src="machbous2.jpg"
+	
+}
+
+function imageout4(img){
+	img.src="loqaimat2.jpg"
+}
+function image4(img){
+	img.src="loqaimat1.jpg"
+	
+}
+
+function imageout5(img){
+	img.src="chebab1.jpg"
+}
+function image5(img){
+	img.src="chebab2.jpg"
+	
+}
+function imageout6(img){
+	img.src="sushi1.jpg"
+}
+function image6(img){
+	img.src="sushi2.jpg"
+	
+}
+
+
+function imageout7(img){
+	img.src="ramen1.jpg"
+}
+function image7(img){
+	img.src="ramen2.jpg"
+	
+}
+
+function imageout8(img){
+	img.src="tempora1.jpg"
+}
+function image8(img){
+	img.src="tempora2.jpg"
+	
+	
+}
+
+function imageout9(img){
+	img.src="kare1.jpg"
+}
+function image9(img){
+	img.src="kare2.jpg"
+	
+}
+
+function imageout10(img){
+	img.src="oko1.jpg"
+}
+function image10(img){
+	img.src="oko2.jpg"
+	
+}
+function imageout11(img){
+	img.src="escargot1.jpg"
+}
+function image11(img){
+	img.src="escargot2.jpg"
+	
+	
+}
+function imageout12(img){
+	img.src="q1.jpg"
+}
+function image12(img){
+	img.src="q2.jpg"
+	
+}
+
+function imageout13(img){
+	img.src="chocolate1.jpg"
+}
+function image13(img){
+	img.src="chocolate2.jpg"
+	
+}
+function imageout14(img){
+	img.src="salmon1.jpg"
+}
+function image14(img){
+	img.src="salmon2.jpg"
+	
+}
+function imageout15(img){
+	img.src="b1.jpg"
+}
+function image15(img){
+	img.src="b2.jpg"
+	
+}
+function imageout16(img){
+	img.src="chicken1.jpg"
+}
+function image16(img){
+	img.src="chicken2.jpg"
+	
+}
+	
+function nasiout17(img){
+	img.src="nasi1.jpg"
+}
+function nasi17(img){
+	img.src="nasi2.jpg"
+}
+
+function laksaout18(img){
+	img.src="laksa1.jpg"
+}
+function laksa18(img){
+	img.src="laksa2.jpg"
+	
+}
+function f1out19(img){
+	img.src="f1.jpg"
+}
+function f119(img){
+	img.src="f2.jpg"
+}
+
+
+function kambout20(img){
+	img.src="kamb1.jpg"
+}
+function kamb20(img){
+	img.src="kamb2.jpg"
+}
+	
+function falafelout21(img){
+	img.src="falafel2.jpg"
+}
+function falafel21(img){
+	img.src="falafel.jpg"
+}
+function kibbehout22(img){
+	img.src="kibbeh1.jpg"
+}
+function kibbeh22(img){
+	img.src="kibbeh2.jpg"
+}
+function shawarmaout23(img){
+	img.src="shawarma1.jpg"
+}
+function shawarma23(img){
+	img.src="shawarma2.jpg"
+}
+
+
+function fattoushout24(img){
+	img.src="fatoush1.jpg"
+}
+function fattoush24(img){
+	img.src="fatoush2.jpg"
+}
+
+function mutabalout25(img){
+	img.src="mutabal1.jpg"
+}
+function mutabal25(img){
+	img.src="mutabal2.jpg"
+}
+
+
+
